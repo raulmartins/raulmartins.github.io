d0f3326b97c24225ad8b778bdbb309e8bd13aca3
Raul Martins <raul.martinslima@gmail.com>
2025-05-25 00:13:54 -0300

    basepah

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: '/portifolio',
+  assetPrefix: '/portifolio/',
   images: {
     unoptimized: true,
     domains: [
